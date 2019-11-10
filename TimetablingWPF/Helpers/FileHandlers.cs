09945d5cf05a8ffab8070d1616bc7b71bd168c56
EdwardW <02ewatine@gmail.com>
2019-11-10 17:46:29 +0000

    Significantly improve + change collections, notably for AddRange. Fixed a bug to do with context menus. Hooray!

--- TimetablingWPF/Helpers/FileHandlers.cs
+++ TimetablingWPF/Helpers/FileHandlers.cs
@@ -12,7 +12,7 @@ namespace TimetablingWPF
 {
     public static class FileHandlers
     {
-        public static void WriteUshortEnum(IEnumerable<ushort> enumerable, BinaryWriter writer)
+        private static void WriteUshortEnum(IEnumerable<ushort> enumerable, BinaryWriter writer)
         {
             IList<ushort> list = enumerable.ToList();
             writer.Write((ushort)list.Count);
@@ -20,6 +20,54 @@ namespace TimetablingWPF
             {
                 writer.Write(list[i]);
             }
+        }
+        private static void WriteBDCEnum<T>(IEnumerable<T> enumerable, BinaryWriter writer, Action<T> action) where T : BaseDataClass
+        {
+            IList<T> list = enumerable.ToList();
+            writer.Write((ushort)list.Count);
+            for (ushort i = 0; i < list.Count; i++)
+            {
+                T obj = list[i];
+                obj.StorageIndex = i;
+                writer.Write(obj.Name);
+                action(obj);
+            }
+        }
+        public static void LoadEnum(Action action, BinaryReader reader)
+        {
+            ushort count = reader.ReadUInt16();
+            for (ushort i = 0; i < count; i++)
+            {
+                action();
+            }
+        }
+        public static IEnumerable<T> LoadEnum<T>(Func<T> func, BinaryReader reader)
+        {
+            ushort count = reader.ReadUInt16();
+            for (ushort i = 0; i < count; i++)
+            {
+                yield return func();
+            }
+        }
+        public static void LoadData(string fpath)
+        {
+            FileStream fileStream = File.OpenRead(fpath);
+            BinaryReader reader = new BinaryReader(fileStream);
+            LoadEnum(() =>
+            {
+                string name = reader.ReadString();
+                YearGroup year = new YearGroup(name);
+                year.Commit();
+            }, reader);
+            LoadEnum(() =>
+            {
+                Teacher teacher = new Teacher() { Name = reader.ReadString() };
+                teacher.UnavailablePeriods.AddRange(LoadEnum<TimetableSlot>(() =>
+                {
+                    ushort unavailable_num = reader.ReadUInt16();
+                    return TimetableSlot.FromInt(unavailable_num);
+                }, reader));
+            }, reader);
         }
         public static void SaveData(string fpath)
         {
@@ -33,71 +81,44 @@ namespace TimetablingWPF
                 year.StorageIndex = i;
                 writer.Write(year.Year);
             }
-            IList<Teacher> teacher_list = GetData<Teacher>();
-            writer.Write((ushort)teacher_list.Count);
-            for (ushort i = 0; i < teacher_list.Count; i++)
+            WriteBDCEnum(GetData<Teacher>(), writer, t =>
             {
-                Teacher teacher = teacher_list[i];
-                teacher.StorageIndex = i;
-                writer.Write(teacher.Name);
-                WriteUshortEnum(teacher.UnavailablePeriods.Select(p => p.ToUshort()), writer);
-            }
-            IList<Subject> subject_list = GetData<Subject>();
-            writer.Write((ushort)subject_list.Count);
-            for (ushort i = 0; i < subject_list.Count; i++)
+                WriteUshortEnum(t.UnavailablePeriods.Select(p => p.ToUshort()), writer);
+            });
+            WriteBDCEnum(GetData<Subject>(), writer, s =>
             {
-                Subject subject = subject_list[i];
-                subject.StorageIndex = i;
-                writer.Write(subject.Name);
-                WriteUshortEnum(subject.Teachers.Select(t => t.StorageIndex), writer);
-            }
-            IList<Form> form_list = GetData<Form>();
-            writer.Write((ushort)form_list.Count);
-            for (ushort i = 0; i < form_list.Count; i++)
+                WriteUshortEnum(s.Teachers.Select(t => t.StorageIndex), writer);
+            });
+            WriteBDCEnum(GetData<Form>(), writer, f =>
             {
-                Form form = form_list[i];
-                form.StorageIndex = i;
-                writer.Write(form.Name);
-                writer.Write(form.YearGroup.StorageIndex);
-            }
-            IList<Room> room_list = GetData<Room>();
-            writer.Write((ushort)room_list.Count);
-            for (ushort i = 0; i < form_list.Count; i++)
+                writer.Write(f.YearGroup.StorageIndex);
+            });
+            WriteBDCEnum(GetData<Room>(), writer, r =>
             {
-                Room room = room_list[i];
-                room.StorageIndex = i;
-                writer.Write(room.Name);
-                writer.Write(room.Quantity);
-                writer.Write(room.Critical);
-            }
-            IList<Group> group_list = GetData<Group>();
-            writer.Write((ushort)group_list.Count);
-            for (ushort i = 0; i < group_list.Count; i++)
+                writer.Write(r.Quantity);
+                writer.Write(r.Critical);
+            });
+            WriteBDCEnum(GetData<Group>(), writer, g =>
             {
-                Group group = group_list[i];
-                writer.Write(group.Name);
-                WriteUshortEnum(group.Subjects.Select(s => s.StorageIndex), writer);
-                WriteUshortEnum(group.Rooms.Select(r => r.StorageIndex), writer);
-            }
-            IList<Lesson> lesson_list = GetData<Lesson>();
-            writer.Write((ushort)lesson_list.Count);
-            for (ushort i = 0; i < lesson_list.Count; i++)
+                WriteUshortEnum(g.Subjects.Select(s => s.StorageIndex), writer);
+                WriteUshortEnum(g.Rooms.Select(r => r.StorageIndex), writer);
+            });
+            WriteBDCEnum(GetData<Lesson>(), writer, l =>
             {
-                Lesson lesson = lesson_list[i];
-                writer.Write(lesson.Name);
-                writer.Write(lesson.Form.StorageIndex);
-                writer.Write(lesson.LessonsPerCycle);
-                writer.Write(lesson.LessonLength);
-                writer.Write(lesson.Subject.StorageIndex);
-                writer.Write(lesson.Assignments.Count);
-                foreach (Assignment assignment in lesson.Assignments)
+                writer.Write(l.Name);
+                writer.Write(l.Form.StorageIndex);
+                writer.Write(l.LessonsPerCycle);
+                writer.Write(l.LessonLength);
+                writer.Write(l.Subject.StorageIndex);
+                writer.Write(l.Assignments.Count);
+                foreach (Assignment assignment in l.Assignments)
                 {
-                    writer.Write(assignment.Lesson.StorageIndex);
+                    writer.Write(assignment.Teacher.StorageIndex);
                     writer.Write(assignment.LessonCount);
                 }
-            }
-            FileStream stream = File.OpenWrite(fpath);
+            });
             all_data.Seek(0, SeekOrigin.Begin);
+            FileStream stream = File.OpenWrite(fpath);
             all_data.CopyTo(stream);
             stream.Close();
             writer.Close();
